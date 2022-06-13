203fc1c7a7b9eba7e8b776e10f2750de0a336b70
twistyfloof <deadmenslegs@gmail.com>
2022-06-13 00:10:03 -0700

    dedupe roles added

--- handlers/roles.py
+++ handlers/roles.py
@@ -1,7 +1,3 @@
-import cmd
-import enum
-import re
-
 from utils import discord
 
 
@@ -14,15 +10,21 @@ def _pretty_roles(role_ids):
 def handle(command, cmd_input, user_id, server_id):
     # Returns a tuple of (output: str, hide_output: bool)
     if command == "add_roles":
+        roles_seen = []
         for _, role in cmd_input.items():
-            discord.add_role(user_id, role, server_id)
-        
-        output = f"Roles {_pretty_roles(cmd_input.values())} have been added.\nAll current roles: {_pretty_roles(discord.get_user_role_ids(server_id, user_id))}"
+            if role not in roles_seen:
+                discord.add_role(user_id, role, server_id)
+                roles_seen.append(role)
+
+        output = f"Roles {_pretty_roles(roles_seen)} have been added.\nAll current roles: {_pretty_roles(discord.get_user_role_ids(server_id, user_id))}"
 
     elif command == "remove_roles":
+        roles_seen = []
         for _, role in cmd_input.items():
-            discord.remove_role(user_id, role, server_id)
-
+            if role not in roles_seen:
+                discord.remove_role(user_id, role, server_id)
+                roles_seen.append(role)
+                
         output = f"Roles {_pretty_roles(cmd_input.values())} have been removed.\n All current roles: {_pretty_roles(discord.get_user_role_ids(server_id, user_id))}"
 
     else:

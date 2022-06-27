938b46c3a9706442c7c2b65e566da79e69ac12ea
zethorix <zethorixx@gmail.com>
2022-06-27 15:50:26 -0700

    Add base honing cpp module

---
+++ cpp_modules/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+
+def main():
+    module_1 = Extension('honing_cpp', sources=['honing_cpp.cpp'])
+
+    setup(name='LostArkTraderPackage',
+          version='1.0',
+          description='Package for the Trader guild Discord bot.',
+          ext_modules=[module_1])
+
+
+if __name__ == '__main__':
+    main()

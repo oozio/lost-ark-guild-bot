817bd28fc0dbc97962cdebd52681b3d3d5846156
oozio <xxue@caltech.edu>
2022-07-27 01:24:47 -0700

    Update render_display.py

--- handlers/render_display.py
+++ handlers/render_display.py
@@ -2,11 +2,8 @@ from . import role_selector, scheduler
 
 # TODO: bad that commands are defined in multiple places
 
-CMD_DISPLAYS = {
-    "role_selector": role_selector.display,
-    "scheduler": scheduler.display
-}
-    
+CMD_DISPLAYS = {"role_selector": role_selector.display, "make_raid": scheduler.display}
+
 
 def display(command):
     return CMD_DISPLAYS[command]

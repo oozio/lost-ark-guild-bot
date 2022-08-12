712f3c722988fad76ce74e4914c06e2eab83de40
oozio <xxue@caltech.edu>
2022-08-11 19:05:55 -0700

    Allow non-alphanumeric characters in voting choices

--- handlers/vote.py
+++ handlers/vote.py
@@ -1,3 +1,6 @@
+from cProfile import label
+import re
+
 from time import sleep
 
 from utils import discord, dynamodb
@@ -8,9 +11,14 @@ PKEY = "vote:{}"
 
 Q_COLUMN = "question"
 CREATOR_COLUMN = "creator"
+LABEL_MAP_COLUMN = "map"
 
 CHOICE_SEPARATOR = "|"
 VOTE_PREFIX = "vote_"
+
+
+def _sanitize(input: str) -> str:
+    return re.sub(r"[^a-zA-Z0-9]", "", input)
 
 
 def _is_vote_column(column_name: str) -> bool:
@@ -21,11 +29,16 @@ def _drop_vote_prefix(column_name: str) -> str:
     return column_name.replace(VOTE_PREFIX, "")
 
 
-def _create_vote(interaction_id, creator, question, votes):
+def _create_vote(interaction_id, creator, question, votes, choice_label_map):
     dynamodb.set_rows(
         VOTES_TABLE,
         PKEY.format(interaction_id),
-        {CREATOR_COLUMN: creator, Q_COLUMN: question, **votes},
+        {
+            CREATOR_COLUMN: creator,
+            Q_COLUMN: question,
+            LABEL_MAP_COLUMN: choice_label_map,
+            **votes,
+        },
     )
 
     sleep(2)
@@ -39,16 +52,19 @@ def _get_votes(interaction_id):
     return dynamodb.get_rows(VOTES_TABLE, PKEY.format(interaction_id))[0]
 
 
-def _get_vote_embed(interaction_id, info):
+def _get_vote_embed(interaction_id):
     vote_info = _get_votes(interaction_id)
 
     question = vote_info.pop(Q_COLUMN, "? ur code is buggy")
+    label_map = vote_info.pop(LABEL_MAP_COLUMN, {})
     vote_fields = []
+
     for key, value in vote_info.items():
         if _is_vote_column(key):
+            pretty_label = label_map.get(key)
             vote_fields.append(
                 {
-                    "name": _drop_vote_prefix(key),
+                    "name": pretty_label,
                     "value": int(value),
                     "inline": True,
                 }
@@ -73,15 +89,23 @@ def display(info: dict) -> dict:
         question = cmd_input["question"]
         choices = cmd_input["choices"].split("|")
 
+        sanitized_choices = []
+        choice_label_map = {}
+        for choice in choices:
+            sanitized_choice = f"{VOTE_PREFIX}{_sanitize(choice)}"
+            sanitized_choices.append(sanitized_choice)
+            choice_label_map[sanitized_choice] = choice
+
         _create_vote(
             interaction_id,
             user_id,
             question,
-            {f"{VOTE_PREFIX}{choice}": 0 for choice in choices},
+            {choice: 0 for choice in sanitized_choices},
+            choice_label_map,
         )
 
         return {
-            "embeds": [_get_vote_embed(interaction_id, info)],
+            "embeds": [_get_vote_embed(interaction_id)],
             "components": vote_view.VoteView(choices).get_buttons(),
         }
     else:
@@ -98,10 +122,15 @@ def handle_button(info):
     message_id = info["base_msg_id"]
     server_id = info["server_id"]
     user_id = info["user_id"]
-    choice = info["data"]["id"]
+    choice = _drop_vote_prefix(info["data"]["id"])
 
-    _update_votes(interaction_id, choice)
+    vote_info = _get_votes(interaction_id)
 
-    new_msg = {"embeds": [_get_vote_embed(interaction_id, info)]}
+    label_map = vote_info.pop(LABEL_MAP_COLUMN, {})
+    for sanitized, pretty in label_map.items():
+        if pretty == choice:
+            _update_votes(interaction_id, sanitized)
+
+    new_msg = {"embeds": [_get_vote_embed(interaction_id)]}
 
     return new_msg
